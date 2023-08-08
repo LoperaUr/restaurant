e30071531bc36816c3a377aa41eada6239851385
Juan Avendano <juanpablo_1810dev@hotmail.com>
2023-08-07 21:26:40 -0500

    Swagger: orderController documnetacion

--- src/main/java/com/pragma/restaurant/controller/OrderController.java
+++ src/main/java/com/pragma/restaurant/controller/OrderController.java
@@ -6,6 +6,8 @@ import com.pragma.restaurant.dto.order.OrderResponseDTO;
 import com.pragma.restaurant.entity.Order;
 import com.pragma.restaurant.service.OrderService;
 import com.pragma.restaurant.util.StateOrder;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/order")
+@Api(value = "Order Management System", description = "Operations related to orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -24,6 +27,7 @@ public class OrderController {
 
 
     @PostMapping("/")
+    @ApiOperation(value = "Create an order")
     ResponseEntity<OrderDTO> create(@RequestBody Order data) {
         try {
             return ResponseEntity
@@ -40,6 +44,7 @@ public class OrderController {
 
 
     @GetMapping("/")
+    @ApiOperation(value = "Filter orders by state and restaurant")
     ResponseEntity<List<OrderResponseDTO>> filterByStateAndRestaurant(
             @RequestParam Character rol,
             @RequestParam String restaurant,
